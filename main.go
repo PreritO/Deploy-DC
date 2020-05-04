b35b7c945e8bb8762920c5a084fa9c3fc61f2c1f
PreritO <PreritO@gcm.ectest.cudevopsfall2018-pg0.wisc.cloudlab.us>
2020-05-04 14:36:51 -0500

    abstracted setupWatcher to a function in same file..

--- main.go
+++ main.go
@@ -243,49 +243,38 @@ func deployer(deploymentPath string, namespace string, clientset *kubernetes.Cli
 	return podNames, nil
 }
 
-// func setupWatcher(podListWatcher *listWatch, queue RateLimitingInterface) *Controller {
-// 	// Bind the workqueue to a cache with the help of an informer. This way we make sure that
-// 	// whenever the cache is updated, the pod key is added to the workqueue.
-// 	// Note that when we finally process the item from the workqueue, we might see a newer version
-// 	// of the Pod than the version which was responsible for triggering the update.
-// 	indexer, informer := cache.NewIndexerInformer(podListWatcher, &v1.Pod{}, 0, cache.ResourceEventHandlerFuncs{
-// 		AddFunc: func(obj interface{}) {
-// 			key, err := cache.MetaNamespaceKeyFunc(obj)
-// 			if err == nil {
-// 				queue.Add(key)
-// 			}
-// 		},
-// 		UpdateFunc: func(old interface{}, new interface{}) {
-// 			key, err := cache.MetaNamespaceKeyFunc(new)
-// 			if err == nil {
-// 				queue.Add(key)
-// 			}
-// 		},
-// 		DeleteFunc: func(obj interface{}) {
-// 			// IndexerInformer uses a delta queue, therefore for deletes we have to use this
-// 			// key function.
-// 			key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
-// 			if err == nil {
-// 				queue.Add(key)
-// 			}
-// 		},
-// 	}, cache.Indexers{})
+func setupWatcher(podListWatcher *cache.ListWatch, queue workqueue.RateLimitingInterface) *Controller {
+	// Bind the workqueue to a cache with the help of an informer. This way we make sure that
+	// whenever the cache is updated, the pod key is added to the workqueue.
+	// Note that when we finally process the item from the workqueue, we might see a newer version
+	// of the Pod than the version which was responsible for triggering the update.
+	indexer, informer := cache.NewIndexerInformer(podListWatcher, &corev1.Pod{}, 0, cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			key, err := cache.MetaNamespaceKeyFunc(obj)
+			if err == nil {
+				queue.Add(key)
+			}
+		},
+		UpdateFunc: func(old interface{}, new interface{}) {
+			key, err := cache.MetaNamespaceKeyFunc(new)
+			if err == nil {
+				queue.Add(key)
+			}
+		},
+		DeleteFunc: func(obj interface{}) {
+			// IndexerInformer uses a delta queue, therefore for deletes we have to use this
+			// key function.
+			key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+			if err == nil {
+				queue.Add(key)
+			}
+		},
+	}, cache.Indexers{})
 
-// 	controller := NewController(queue, indexer, informer)
+	controller := NewController(queue, indexer, informer)
 
-// 	// // We can now warm up the cache for initial synchronization.
-// 	// // Let's suppose that we knew about a pod "mypod" on our last run, therefore add it to the cache.
-// 	// // If this pod is not there anymore, the controller will be notified about the removal after the
-// 	// // cache has synchronized.
-// 	// indexer.Add(&v1.Pod{
-// 	// 	ObjectMeta: meta_v1.ObjectMeta{
-// 	// 		Name:      "mypod",
-// 	// 		Namespace: v1.NamespaceDefault,
-// 	// 	},
-// 	// })
-
-// 	return controller
-// }
+	return controller
+}
 
 func main() {
 	// First, we parse the application definition file for app statisitics
@@ -333,32 +322,7 @@ func main() {
 	podListWatcher := cache.NewListWatchFromClient(clientset.CoreV1().RESTClient(), "pods", namespace.(string) , fields.Everything())
 	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
 
-	//controller := setupWatcher(podListWatcher, queue)
-
-	indexer, informer := cache.NewIndexerInformer(podListWatcher, &corev1.Pod{}, 0, cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			key, err := cache.MetaNamespaceKeyFunc(obj)
-			if err == nil {
-				queue.Add(key)
-			}
-		},
-		UpdateFunc: func(old interface{}, new interface{}) {
-			key, err := cache.MetaNamespaceKeyFunc(new)
-			if err == nil {
-				queue.Add(key)
-			}
-		},
-		DeleteFunc: func(obj interface{}) {
-			// IndexerInformer uses a delta queue, therefore for deletes we have to use this
-			// key function.
-			key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
-			if err == nil {
-				queue.Add(key)
-			}
-		},
-	}, cache.Indexers{})
-
-	controller := NewController(queue, indexer, informer)
+	controller := setupWatcher(podListWatcher, queue)
 
 	// Now let's start the controller
 	stop := make(chan struct{})
