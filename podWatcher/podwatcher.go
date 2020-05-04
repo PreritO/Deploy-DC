4c28ee6dbd9d67d6f393d5df3558905ace8a66d9
PreritO <PreritO@gcm.ectest.cudevopsfall2018-pg0.wisc.cloudlab.us>
2020-05-04 14:29:45 -0500

    updated main.go to include pod watcher from Medium article without abstraction and changing the Queue type - working to deploy deathstar app + monitoring pods being added/removed

--- podWatcher/podwatcher.go
+++ podWatcher/podwatcher.go
@@ -2,20 +2,15 @@
 package podWatcher
 
 import (
-	"flag"
 	"fmt"
 	"time"
 
 	"k8s.io/klog"
 
 	"k8s.io/api/core/v1"
-	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/workqueue"
 )
 
